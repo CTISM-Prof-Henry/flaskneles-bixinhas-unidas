86c8eac31277373627615b8982d92a4ef49c2d4b
luize Baldoni <oliveira.luize@acad.ufsm.br>
2022-12-06 11:01:39 -0300

    alterações no frontend

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,27 @@
 function alerta_vermelho() {
     window.alert('Olá mundo! Eu sou uma função Javascript definida no script app/static/js/main.js');
 }
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links a");
+  
+    burger.addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+  
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.5
+          }s `;
+        }
+      });
+      burger.classList.toggle("toggle");
+    });
+    //
+  };
+  
+  navSlide();
+  
