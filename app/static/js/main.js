133d2c088668c9e622c1217b2c69986ef380a777
luize Baldoni <oliveira.luize@acad.ufsm.br>
2022-12-30 19:12:26 -0300

    alterações no css e js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -6,7 +6,6 @@ const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links a");
-  
     burger.addEventListener("click", () => {
       nav.classList.toggle("nav-active");
   
