12e4220f702acf1800189bf2128cb67105915c95
luize Baldoni <oliveira.luize@acad.ufsm.br>
2022-12-30 18:53:51 -0300

    alteração na pgina inicial, javascript

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -22,3 +22,19 @@ const navSlide = () => {
       burger.classList.toggle("toggle");
     });
   }
+const modal = () =>{
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
