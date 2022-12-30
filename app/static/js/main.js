0ee186c9df17931bfa9daae4448c75f5f827235b
luize Baldoni <oliveira.luize@acad.ufsm.br>
2022-12-30 16:32:49 -0300

    font google adicionada

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -23,22 +23,3 @@ const navSlide = () => {
     });
   };
 // ADICIONANDO MODAL DA PAGINA INICIAL
-const switchModal = () => {
-  const modal = document.querySelector('.modal')
-  const actualStyle = modal.style.display
-  if(actualStyle == 'block') {
-    modal.style.display = 'none'
-  }
-  else {
-    modal.style.display = 'block'
-  }
-}
-const btn = document.querySelector('.modalBtn')
-btn.addEventListener('click', switchModal)
-
-window.onclick = function(event) {
-    const modal = document.querySelector('.modal')
-  if (event.target == modal) {
-    switchModal()
-  }
-}
